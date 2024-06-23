263927252b1121b874e9b6efa5b5e18859007bc5
kitarou888 <85793702+kitarou888@users.noreply.github.com>
2024-06-23 19:41:56 +0900

    通しテスト完了（平均気温と最高気温を同時表示）

--- 99.npm/index.js
+++ 99.npm/index.js
@@ -4,39 +4,55 @@ const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const url1 =
-    "https://www.data.jma.go.jp/gmd/cpd/monitor/dailyview/graph_mkhtml_d.php?&n=47662&p=91&s=3&r=2&y=2018&m=6&d=30&e=0&k=0";
-  const url2 =
-    "https://www.data.jma.go.jp/gmd/cpd/monitor/dailyview/graph_mkhtml_d.php?&n=47662&p=91&s=3&r=2&y=2017&m=6&d=30&e=0&k=0";
+    "https://www.data.jma.go.jp/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year=2021&month=08";
+  // const url1 =
+  //   "https://zenn.dev/nobokko/articles/idea_jma_prec_no_and_block_no";
+  // const url2 =
+  //   "https://www.data.jma.go.jp/gmd/cpd/monitor/dailyview/graph_mkhtml_d.php?&n=47662&p=91&s=3&r=2&y=2017&m=6&d=30&e=0&k=0";
 
   await page.goto(url1);
 
-  const dates = (
-    await page.$$eval("th", (list) =>
-      list.map((e) => e.textContent.trim().replace(/\d{4}年/i, "")),
-    )
-  ).slice(6);
+  // const dates = (
+  //   await page.$$eval("th", (list) => list.map((e) => e.textContent))
+  // ).slice(0);
 
   const data1 = (
     await page.$$eval("td", (list) => list.map((e) => e.textContent))
-  ).slice(4);
+  ).slice(22, -4);
+  // .slice(22);
+  // console.log(data1);
 
-  const averageTemperatures1 = data1.filter((v, i) => i % 4 === 0);
-  const highestTemperatures1 = data1.filter((v, i) => i % 4 === 1);
-
-  await page.goto(url2);
-
-  const data2 = (
-    await page.$$eval("td", (list) => list.map((e) => e.textContent))
-  ).slice(4);
-
-  const averageTemperatures2 = data2.filter((v, i) => i % 4 === 0);
-  const highestTemperatures2 = data2.filter((v, i) => i % 4 === 1);
-
-  console.log(dates);
+  const averageTemperatures1 = data1.filter((v, i) => i % 21 === 6);
   console.log(averageTemperatures1);
-  console.log(highestTemperatures1);
-  console.log(averageTemperatures2);
-  console.log(highestTemperatures2);
+
+  // const data1 = (
+  //   await page.$$eval("td", (list) => list.map((e) => e.textContent))
+  // ).slice(183);
+
+  // const precNos = data1.filter((v, i) => i % 3 === 0);
+  // const blockNos = data1.filter((v, i) => i % 3 === 1);
+  // const names = data1.filter((v, i) => i % 3 === 2);
+
+  // console.log(names.length);
+  // for (let i = 0; i < 1794; i++) {
+  //   console.log(
+  //     `${names[i]}:{precNo: ${precNos[i]}, blockNo: "${blockNos[i]}",},`,
+  //   );
+  // }
+
+  // await page.goto(url2);
+
+  // const data2 = (
+  //   await page.$$eval("td", (list) => list.map((e) => e.textContent))
+  // ).slice(4);
+
+  // const averageTemperatures2 = data2.filter((v, i) => i % 4 === 0);
+  // const highestTemperatures2 = data2.filter((v, i) => i % 4 === 1);
+
+  // console.log(dates);
+  // console.log(highestTemperatures1);
+  // console.log(averageTemperatures2);
+  // console.log(highestTemperatures2);
 
   await browser.close();
 };
--- 99.npm/main.js
+++ 99.npm/main.js
@@ -1,15 +1,17 @@
 import express from "express";
 import open from "open";
-import axios from "axios";
-import https from "https";
 import enquirer from "enquirer";
-import { prefectures } from "./prefecture.js";
-import { cities } from "./city.js";
+import puppeteer from "puppeteer";
+import { prefectures } from "./prefectures.js";
+import { cities } from "./cities.js";
+import { months } from "./months.js";
 
 const { Select } = enquirer;
+const minYear = 1976;
+
 const promptPrefecture = new Select({
   name: "value",
-  message: "都道府県を選んでください。",
+  message: "都道府県または地方を選んでください。",
   choices: prefectures,
 });
 
@@ -22,10 +24,11 @@ const promptCity = new Select({
 });
 
 const city = await promptCity.run();
-const pointNumber = cities[prefecture][city];
+const precNo = cities[prefecture][city].precNo;
+const blockNo = cities[prefecture][city].blockNo;
 
-let baseYears = ["本日"];
-for (let year = new Date().getFullYear(); year >= 1976; year--) {
+const baseYears = [];
+for (let year = new Date().getFullYear(); year >= minYear + 1; year--) {
   baseYears.push(`${year} 年`);
 }
 
@@ -35,70 +38,89 @@ const promptBaseYear = new Select({
   choices: baseYears,
 });
 
-let baseYear, month;
-const answerBaseYear = await promptBaseYear.run();
-if (answerBaseYear === "本日") {
-  const today = new Date();
-  baseYear = today.getFullYear();
-  month = today.getMonth() + 1;
-} else {
-  baseYear = parseInt(answerBaseYear);
+const baseYearLabel = await promptBaseYear.run();
+const baseYear = baseYearLabel.replace(/[^0-9]/g, "");
 
-  const months = [
-    " 1 月",
-    " 2 月",
-    " 3 月",
-    " 4 月",
-    " 5 月",
-    " 6 月",
-    " 7 月",
-    " 8 月",
-    " 9 月",
-    "10 月",
-    "11 月",
-    "12 月",
-  ];
-  const promptMonth = new Select({
-    name: "value",
-    message: "基準月を選んでください。",
-    choices: months,
-  });
-
-  month = parseInt(await promptMonth.run());
+const targetYears = [];
+for (let year = baseYear - 1; year >= minYear; year--) {
+  targetYears.push(`${year} 年`);
 }
 
-const comparisonPeriods = [" 1 年前", " 5 年前", "10 年前", "30 年前"];
-const promptComparisonPeriod = new Select({
+const promptTargetYear = new Select({
   name: "value",
-  message: "何年前の同月と比較しますか？",
-  choices: comparisonPeriods,
+  message: "比較年を選んでください。",
+  choices: targetYears,
 });
 
-const comparisonPeriod = parseInt(await promptComparisonPeriod.run());
-const targetYear = Math.max(baseYear - comparisonPeriod, 1970);
+const targetYearLabel = await promptTargetYear.run();
+const targetYear = targetYearLabel.replace(/[^0-9]/g, "");
 
-const baseURL = `https://api.cultivationdata.net/past?no=${pointNumber}&year=${baseYear}&month=${month}`;
-const targetURL = `https://api.cultivationdata.net/past?no=${pointNumber}&year=${targetYear}&month=${month}`;
-
-const baseOptions = {
-  method: "GET",
-  headers: { "content-type": "application/x-www-form-urlencoded" },
-  url: baseURL,
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-};
-const targetOptions = {
-  method: "GET",
-  headers: { "content-type": "application/x-www-form-urlencoded" },
-  url: targetURL,
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-};
-
-const responses = await Promise.all([axios(baseOptions), axios(targetOptions)]);
-const labels = Object.keys(responses[0].data[city]);
-
-const data = responses.map((response) => {
-  return Object.values(response.data[city]).map((obj) => obj["平均気温"]);
+const promptMonth = new Select({
+  name: "value",
+  message: "基準月を選んでください。",
+  choices: months,
 });
+
+const monthLabel = await promptMonth.run();
+const month = monthLabel.replace(/[^0-9]/g, "").padStart(2, "0");
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+const pageType = blockNo.length === 5 ? "s1" : "a1";
+const columnNum = pageType === "s1" ? 21 : 18;
+const averageTemperatureColumnIndex = pageType === "s1" ? 6 : 4;
+const highestTemperatureColumnIndex = pageType === "s1" ? 7 : 5;
+
+const baseURL = `https://www.data.jma.go.jp/stats/etrn/view/daily_${pageType}.php?prec_no=${precNo}&block_no=${blockNo}&year=${baseYear}&month=${month}`;
+const targetURL = `https://www.data.jma.go.jp/stats/etrn/view/daily_${pageType}.php?prec_no=${precNo}&block_no=${blockNo}&year=${targetYear}&month=${month}`;
+
+await page.goto(baseURL);
+
+const baseData = (
+  await page.$$eval("td", (list) => list.map((e) => e.textContent))
+).slice(22, -4);
+
+// console.log(baseData);
+
+const labels = baseData
+  .filter((v, i) => i % columnNum === 0)
+  .map((v) => v + "日");
+
+// console.log(labels);
+
+const baseAverageTemperatures = baseData.filter(
+  (v, i) => i % columnNum === averageTemperatureColumnIndex,
+);
+
+const baseHighestTemperatures = baseData.filter(
+  (v, i) => i % columnNum === highestTemperatureColumnIndex,
+);
+
+// console.log(baseAverageTemperatures);
+
+await page.goto(targetURL);
+
+const targetData = (
+  await page.$$eval("td", (list) => list.map((e) => e.textContent))
+).slice(22, -4);
+
+const targetAverageTemperatures = targetData.filter(
+  (v, i) => i % columnNum === averageTemperatureColumnIndex,
+);
+
+const targetHighestTemperatures = targetData.filter(
+  (v, i) => i % columnNum === highestTemperatureColumnIndex,
+);
+
+const averageTemperaturedata = [
+  baseAverageTemperatures,
+  targetAverageTemperatures,
+];
+const highestTemperaturedata = [
+  baseHighestTemperatures,
+  targetHighestTemperatures,
+];
 
 const app = express();
 const port = 3000;
@@ -116,8 +138,11 @@ app.get("/", (req, res) => {
   <body>
     <h1 id="title"></h1>
     <canvas id="myChart" width="300" height="100"></canvas>
+    <canvas id="myChart2" width="300" height="100"></canvas>
     <script>
-      const text = "${city} の温暖化傾向（${comparisonPeriod}年前）";
+      const text = "${city} の温暖化傾向（上：平均気温　下：最高気温）";
+      const baseLabel = "${baseYearLabel + monthLabel}";
+      const targetLabel = "${targetYearLabel + monthLabel}";
       document.getElementById("title").textContent = text;
       var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
@@ -125,16 +150,36 @@ app.get("/", (req, res) => {
         data: {
           labels: ${JSON.stringify(labels)},
           datasets: [{
-            label: '今',
+            label: baseLabel,
             backgroundColor: 'rgb(0, 0, 0)',
             borderColor: 'rgb(255, 0, 0)',
-            data: ${JSON.stringify(data[0])}, 
+            data: ${JSON.stringify(averageTemperaturedata[0])},
           },
           {
-            label: '昔',
+            label: targetLabel,
             backgroundColor: 'rgb(0, 0, 0)',
             borderColor: 'rgb(0, 0, 255)',
-            data: ${JSON.stringify(data[1])}, 
+            data: ${JSON.stringify(averageTemperaturedata[1])},
+          }]
+        },
+        options: {}
+      });
+      var ctx2 = document.getElementById('myChart2').getContext('2d');
+      var myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+          labels: ${JSON.stringify(labels)},
+          datasets: [{
+            label: baseLabel,
+            backgroundColor: 'rgb(0, 0, 0)',
+            borderColor: 'rgb(255, 0, 0)',
+            data: ${JSON.stringify(highestTemperaturedata[0])},
+          },
+          {
+            label: targetLabel,
+            backgroundColor: 'rgb(0, 0, 0)',
+            borderColor: 'rgb(0, 0, 255)',
+            data: ${JSON.stringify(highestTemperaturedata[1])},
           }]
         },
         options: {}
