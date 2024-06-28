aae9a669f04c7dbae8d0e207d43842ce8c5fddc2
kitarou888 <85793702+kitarou888@users.noreply.github.com>
2024-06-29 08:24:51 +0900

    リファクタリング（SurveyPointクラスにメイン処理を切り出し）

--- 99.npm/main.js
+++ 99.npm/main.js
@@ -2,197 +2,206 @@ import express from "express";
 import open from "open";
 import enquirer from "enquirer";
 import puppeteer from "puppeteer";
-import { prefectures } from "./prefectures.js";
 import { cities } from "./cities.js";
 import { months } from "./months.js";
+import { SurveyPoint } from "./surveyPoint.js";
 
 const { Select } = enquirer;
 const minYear = 1976;
+const surveyDataStartIndex = 22;
+const surveyDataEndIndex = -4;
 
-const promptPrefecture = new Select({
-  name: "value",
-  message: "都道府県または地方を選んでください。",
-  choices: prefectures,
-});
+const main = async () => {
+  const promptPrefecture = new Select({
+    name: "value",
+    message: "都道府県または地方を選んでください。",
+    choices: Object.keys(cities),
+  });
 
-const prefecture = await promptPrefecture.run();
+  const prefecture = await promptPrefecture.run();
 
-const promptCity = new Select({
-  name: "value",
-  message: "市町村を選んでください。",
-  choices: Object.keys(cities[prefecture]),
-});
+  const promptCity = new Select({
+    name: "value",
+    message: "市町村を選んでください。",
+    choices: Object.keys(cities[prefecture]),
+  });
 
-const city = await promptCity.run();
-const precNo = cities[prefecture][city].precNo;
-const blockNo = cities[prefecture][city].blockNo;
+  const city = await promptCity.run();
+  const baseYears = [];
+  for (let year = new Date().getFullYear(); year >= minYear + 1; year--) {
+    baseYears.push(`${year} 年`);
+  }
 
-const baseYears = [];
-for (let year = new Date().getFullYear(); year >= minYear + 1; year--) {
-  baseYears.push(`${year} 年`);
-}
+  const promptBaseYear = new Select({
+    name: "value",
+    message: "基準年を選んでください。",
+    choices: baseYears,
+  });
 
-const promptBaseYear = new Select({
-  name: "value",
-  message: "基準年を選んでください。",
-  choices: baseYears,
-});
+  const baseYear = (await promptBaseYear.run()).replace(/[^0-9]/g, "");
 
-const baseYearLabel = await promptBaseYear.run();
-const baseYear = baseYearLabel.replace(/[^0-9]/g, "");
+  const targetYears = [];
+  for (let year = baseYear - 1; year >= minYear; year--) {
+    targetYears.push(`${year} 年`);
+  }
 
-const targetYears = [];
-for (let year = baseYear - 1; year >= minYear; year--) {
-  targetYears.push(`${year} 年`);
-}
+  const promptTargetYear = new Select({
+    name: "value",
+    message: "比較年を選んでください。",
+    choices: targetYears,
+  });
 
-const promptTargetYear = new Select({
-  name: "value",
-  message: "比較年を選んでください。",
-  choices: targetYears,
-});
+  const targetYear = (await promptTargetYear.run()).replace(/[^0-9]/g, "");
 
-const targetYearLabel = await promptTargetYear.run();
-const targetYear = targetYearLabel.replace(/[^0-9]/g, "");
+  const promptMonth = new Select({
+    name: "value",
+    message: "基準月を選んでください。",
+    choices: months,
+  });
 
-const promptMonth = new Select({
-  name: "value",
-  message: "基準月を選んでください。",
-  choices: months,
-});
+  const month = (await promptMonth.run()).replace(/[^0-9]/g, "");
 
-const monthLabel = await promptMonth.run();
-const month = monthLabel.replace(/[^0-9]/g, "").padStart(2, "0");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+  const precNo = cities[prefecture][city].precNo;
+  const blockNo = cities[prefecture][city].blockNo;
 
-const pageType = blockNo.length === 5 ? "s1" : "a1";
-const columnNum = pageType === "s1" ? 21 : 18;
-const averageTemperatureColumnIndex = pageType === "s1" ? 6 : 4;
-const highestTemperatureColumnIndex = pageType === "s1" ? 7 : 5;
+  const baseSurveyPoint = new SurveyPoint({
+    precNo,
+    blockNo,
+    year: baseYear,
+    month,
+  });
+  const targetSurveyPoint = new SurveyPoint({
+    precNo,
+    blockNo,
+    year: targetYear,
+    month,
+  });
 
-const baseURL = `https://www.data.jma.go.jp/stats/etrn/view/daily_${pageType}.php?prec_no=${precNo}&block_no=${blockNo}&year=${baseYear}&month=${month}`;
-const targetURL = `https://www.data.jma.go.jp/stats/etrn/view/daily_${pageType}.php?prec_no=${precNo}&block_no=${blockNo}&year=${targetYear}&month=${month}`;
+  await page.goto(baseSurveyPoint.url());
 
-await page.goto(baseURL);
+  const baseData = (
+    await page.$$eval("td", (elements) => elements.map((e) => e.textContent))
+  ).slice(surveyDataStartIndex, surveyDataEndIndex);
 
-const baseData = (
-  await page.$$eval("td", (list) => list.map((e) => e.textContent))
-).slice(22, -4);
+  const totalColumns = baseSurveyPoint.totalColumns();
+  const dateIndex = 0;
+  const averageTemperatureIndex = baseSurveyPoint.averageTemperatureIndex();
+  const highestTemperatureIndex = baseSurveyPoint.highestTemperatureIndex();
 
-// console.log(baseData);
+  const labels = baseData
+    .filter((_, i) => i % totalColumns === dateIndex)
+    .map((v) => v + "日");
 
-const labels = baseData
-  .filter((v, i) => i % columnNum === 0)
-  .map((v) => v + "日");
+  const baseAverageTemperatures = baseData.filter(
+    (_, i) => i % totalColumns === averageTemperatureIndex,
+  );
 
-// console.log(labels);
+  const baseHighestTemperatures = baseData.filter(
+    (_, i) => i % totalColumns === highestTemperatureIndex,
+  );
 
-const baseAverageTemperatures = baseData.filter(
-  (v, i) => i % columnNum === averageTemperatureColumnIndex,
-);
+  await page.goto(targetSurveyPoint.url());
 
-const baseHighestTemperatures = baseData.filter(
-  (v, i) => i % columnNum === highestTemperatureColumnIndex,
-);
+  const targetData = (
+    await page.$$eval("td", (elements) => elements.map((e) => e.textContent))
+  ).slice(surveyDataStartIndex, surveyDataEndIndex);
 
-// console.log(baseAverageTemperatures);
+  await browser.close();
 
-await page.goto(targetURL);
+  const targetAverageTemperatures = targetData.filter(
+    (_, i) => i % totalColumns === averageTemperatureIndex,
+  );
 
-const targetData = (
-  await page.$$eval("td", (list) => list.map((e) => e.textContent))
-).slice(22, -4);
+  const targetHighestTemperatures = targetData.filter(
+    (_, i) => i % totalColumns === highestTemperatureIndex,
+  );
 
-const targetAverageTemperatures = targetData.filter(
-  (v, i) => i % columnNum === averageTemperatureColumnIndex,
-);
+  const averageTemperaturedata = [
+    baseAverageTemperatures,
+    targetAverageTemperatures,
+  ];
 
-const targetHighestTemperatures = targetData.filter(
-  (v, i) => i % columnNum === highestTemperatureColumnIndex,
-);
+  const highestTemperaturedata = [
+    baseHighestTemperatures,
+    targetHighestTemperatures,
+  ];
 
-const averageTemperaturedata = [
-  baseAverageTemperatures,
-  targetAverageTemperatures,
-];
-const highestTemperaturedata = [
-  baseHighestTemperatures,
-  targetHighestTemperatures,
-];
+  const app = express();
+  const port = 3000;
 
-const app = express();
-const port = 3000;
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.get("/", (_, res) => {
+    res.send(`
+      <html>
+      <head>
+        <title>GlobalWarmingSurvey</title>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      </head>
+      <body>
+        <h1 id="title"></h1>
+        <canvas id="myChart" width="300" height="100"></canvas>
+        <canvas id="myChart2" width="300" height="100"></canvas>
+        <script>
+          const text = "${city} の温暖化傾向（上：平均気温　下：最高気温）";
+          const baseLabel = "${baseYear} 年 ${month} 月";
+          const targetLabel = "${targetYear} 年 ${month} 月";
+          document.getElementById("title").textContent = text;
+          var ctx = document.getElementById('myChart').getContext('2d');
+          var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: ${JSON.stringify(labels)},
+              datasets: [{
+                label: baseLabel,
+                backgroundColor: 'rgb(0, 0, 0)',
+                borderColor: 'rgb(255, 0, 0)',
+                data: ${JSON.stringify(averageTemperaturedata[0])},
+              },
+              {
+                label: targetLabel,
+                backgroundColor: 'rgb(0, 0, 0)',
+                borderColor: 'rgb(0, 0, 255)',
+                data: ${JSON.stringify(averageTemperaturedata[1])},
+              }]
+            },
+            options: {}
+          });
+          var ctx2 = document.getElementById('myChart2').getContext('2d');
+          var myChart2 = new Chart(ctx2, {
+            type: 'line',
+            data: {
+              labels: ${JSON.stringify(labels)},
+              datasets: [{
+                label: baseLabel,
+                backgroundColor: 'rgb(0, 0, 0)',
+                borderColor: 'rgb(255, 0, 0)',
+                data: ${JSON.stringify(highestTemperaturedata[0])},
+              },
+              {
+                label: targetLabel,
+                backgroundColor: 'rgb(0, 0, 0)',
+                borderColor: 'rgb(0, 0, 255)',
+                data: ${JSON.stringify(highestTemperaturedata[1])},
+              }]
+            },
+            options: {}
+          });
+        </script>
+      </body>
+      </html>
+    `);
+  });
 
-app.get("/", (req, res) => {
-  res.send(`
-  <html>
-  <head>
-    <title>GlobalWarmingSurvey</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  </head>
-  <body>
-    <h1 id="title"></h1>
-    <canvas id="myChart" width="300" height="100"></canvas>
-    <canvas id="myChart2" width="300" height="100"></canvas>
-    <script>
-      const text = "${city} の温暖化傾向（上：平均気温　下：最高気温）";
-      const baseLabel = "${baseYearLabel + monthLabel}";
-      const targetLabel = "${targetYearLabel + monthLabel}";
-      document.getElementById("title").textContent = text;
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ${JSON.stringify(labels)},
-          datasets: [{
-            label: baseLabel,
-            backgroundColor: 'rgb(0, 0, 0)',
-            borderColor: 'rgb(255, 0, 0)',
-            data: ${JSON.stringify(averageTemperaturedata[0])},
-          },
-          {
-            label: targetLabel,
-            backgroundColor: 'rgb(0, 0, 0)',
-            borderColor: 'rgb(0, 0, 255)',
-            data: ${JSON.stringify(averageTemperaturedata[1])},
-          }]
-        },
-        options: {}
-      });
-      var ctx2 = document.getElementById('myChart2').getContext('2d');
-      var myChart2 = new Chart(ctx2, {
-        type: 'line',
-        data: {
-          labels: ${JSON.stringify(labels)},
-          datasets: [{
-            label: baseLabel,
-            backgroundColor: 'rgb(0, 0, 0)',
-            borderColor: 'rgb(255, 0, 0)',
-            data: ${JSON.stringify(highestTemperaturedata[0])},
-          },
-          {
-            label: targetLabel,
-            backgroundColor: 'rgb(0, 0, 0)',
-            borderColor: 'rgb(0, 0, 255)',
-            data: ${JSON.stringify(highestTemperaturedata[1])},
-          }]
-        },
-        options: {}
-      });
-    </script>
-  </body>
-  </html>
+  app.listen(port, () => {
+    console.log(`Server is runnnin on port ${port}`);
+  });
 
-  `);
-});
+  open(`http://localhost:${port}`);
+};
 
-app.listen(port, () => {
-  console.log(`Server is runnnin on port ${port}`);
-});
-
-open(`http://localhost:${port}`);
+main();
---
+++ 99.npm/surveyPoint.js
@@ -0,0 +1,48 @@
+const majorBlockNoDigit = 5;
+const majorPageType = "s1";
+const minorPageType = "a1";
+const majorPageTotalColumns = 21;
+const minorPageTotalColumns = 18;
+const majorPageAverageTemperatureIndex = 6;
+const minorPageAverageTemperatureIndex = 4;
+const majorPageHighestTemperatureIndex = 7;
+const minorPageHighestTemperatureIndex = 5;
+
+export class SurveyPoint {
+  constructor(obj) {
+    this.precNo = obj.precNo;
+    this.blockNo = obj.blockNo;
+    this.year = obj.year;
+    this.month = obj.month;
+  }
+
+  pageType() {
+    return this.blockNo.length === majorBlockNoDigit
+      ? majorPageType
+      : minorPageType;
+  }
+
+  url() {
+    return `https://www.data.jma.go.jp/stats/etrn/view/daily_${this.pageType()}.php?prec_no=${
+      this.precNo
+    }&block_no=${this.blockNo}&year=${this.year}&month=${this.month}`;
+  }
+
+  totalColumns() {
+    return this.blockNo.length === majorBlockNoDigit
+      ? majorPageTotalColumns
+      : minorPageTotalColumns;
+  }
+
+  averageTemperatureIndex() {
+    return this.blockNo.length === majorBlockNoDigit
+      ? majorPageAverageTemperatureIndex
+      : minorPageAverageTemperatureIndex;
+  }
+
+  highestTemperatureIndex() {
+    return this.blockNo.length === majorBlockNoDigit
+      ? majorPageHighestTemperatureIndex
+      : minorPageHighestTemperatureIndex;
+  }
+}
